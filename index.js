8017dcf20685e0ee6f5a064f0de95ca7759fe83b
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-04 11:15:14 +0300

    commit tree a623fd061f6cae4517b9b3f05a275079f0c0214b

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+  return (a + b);
+}
+//console.log(add(1000, 111));
+
+function subtract(a, b){
+  return a - b;
+}
+//console.log(subtract(500 - 14)));
+
+
+
+function multiply(a, b){
+  return a * b;
+}
+//console.log(a * b);
+function divide( a, b){
+  return a / b;
+}
+//console.log(a, b)
+
+
+function increment(n){
+  n++;
+  return(n);
+}
+
+function decrement(n){
+  n--;
+  return(n);
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n, 10);
+}
